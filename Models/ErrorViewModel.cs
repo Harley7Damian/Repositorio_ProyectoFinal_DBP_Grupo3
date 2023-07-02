4039c52d1de90afd5f8b6c92af8722ebf3e268c8
Harley7Damian <116583854+Harley7Damian@users.noreply.github.com>
2023-07-01 21:00:35 -0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoDesarrollo.B.P.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
